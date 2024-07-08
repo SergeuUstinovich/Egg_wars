bc7f81674ce1cf8ac82e992f0891183034000da2
unknown <UstinovichSergey>
2024-07-08 21:04:50 +0300

    circleRun

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,35 +1,42 @@
 import { useState } from "react";
 import Canvas from "../Canvas/Canvas";
 import style from "./Layout.module.scss";
-import useCanvas from "../../utils/useCanvas";
 
 interface circlePositionProps {
-    x: number,
-    y: number
+  x: number,
+  y: number,
+  dx: number,
+  dy: number
 }
 
 function Layout() {
     const [score, setScore] = useState(0);
-    const [numCircles, setNumCircles] = useState(0);
+    const [numCircles, setNumCircles] = useState(50);
     const [circlePosition, setCirclePosition] = useState<circlePositionProps[]>([]);
-    // const canvasRef = useCanvas(draw)
-    // const canvas = canvasRef.current;
-    // const width = canvas?.width;
-    // const height = canvas?.height;
-    // console.log(width, height) //undefined
     function getRandomCoordinate(max: number) {
         return Math.random() * max;
     }
-
+    const speedRun = 100
     
+    function generateRandomPosition() {
+      const minX = 50; // Начало области по X
+      const maxX = 320; // Конец области по X
+      const minY = 300;  // Начало области по Y
+      const maxY = 400; // Конец области по Y
 
-    // function addCircle() {
-    //     setCirclePosition((prevPositions) => [...prevPositions, generateRandomPosition()]);
-    // }
+    const randomX = getRandomCoordinate(maxX - minX) + minX;
+    const randomY = getRandomCoordinate(maxY - minY) + minY;
+      return { x: randomX, y: randomY };
+    }
+
+    function addCircle() {
+      const position = generateRandomPosition();
+      const dx = (360 / 2 - position.x) / speedRun;
+      const dy = (480 / 2.5 - position.y) / speedRun;
+      setCirclePosition((prevPositions) => [...prevPositions, { ...position, dx, dy }]);
+    }
 
     function draw(ctx: CanvasRenderingContext2D, frameCount: number) {
-    // const sizes = resizeCanvas(ctx.canvas);
-    // console.log(sizes)
     const canvas = ctx.canvas;
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
@@ -45,19 +52,21 @@ function Layout() {
       rectHeight
     );
 
-    function generateRandomPosition() {
-        const randomX = getRandomCoordinate(canvas.width);
-        const randomY = getRandomCoordinate(canvas.height);
-        return { x: randomX, y: randomY };
-    }
+    circlePosition.map((position, index) => {
+      position.x += position.dx;
+      position.y += position.dy;
 
-    circlePosition.map((position) => {
-        drawCircle(position.x, position.y);
-      });
+      // Проверяем, достиг ли круг квадрата
+      if (position.x >= centerX / 2 - rectWidth / 2 && position.x <= centerX / 2 + rectWidth / 2 &&
+          position.y >= centerY / 2.5 - rectHeight && position.y <= centerY / 2.5) {
+          setScore(prevScore => prevScore + 1);
+          setCirclePosition(prevPositions => prevPositions.filter((_, i) => i !== index));
+      } else {
+          drawCircle(position.x, position.y);
+      }
+  });
 
     // Генерируем случайные координаты для кружков
- 
-
     function drawCircle(x: number, y: number) {
         const circleRadius = 10;
         ctx.fillStyle = "red";
@@ -68,16 +77,19 @@ function Layout() {
     }
 
     const handleCircle = () => {
-        // addCircle()
-        setNumCircles(prev => prev + 1)
+      if (numCircles > 0) {
+        addCircle();
+        setNumCircles(prev => prev - 1);
+    }
     }
 
   return (
     <>
       <header></header>
       <main className={style.main}>
+        <div>{score} Монеты</div>
         <Canvas draw={draw} />
-        <button className={style.btnUnit} onClick={handleCircle}>Клик</button>
+        <button className={style.btnUnit} onClick={handleCircle}>Клик {numCircles}</button>
       </main>
       <footer></footer>
     </>
