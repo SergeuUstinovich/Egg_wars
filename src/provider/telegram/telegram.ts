16ee2f7145f49c40787e082c9d8f339336c1d256
unknown <UstinovichSergey>
2024-07-17 18:08:46 +0300

    fixid

--- src/provider/telegram/telegram.ts
+++ src/provider/telegram/telegram.ts
@@ -6,7 +6,7 @@ declare global {
 
 export const useTelegram = () => {
   const tg = window.Telegram.WebApp;
-  const userName = tg?.initDataUnsafe?.user?.username;
-  const tg_id = tg?.initDataUnsafe?.user?.tg_id
+  const userName = tg?.initDataUnsa?.user?.username;
+  const tg_id = tg?.initDataUnsafe?.user?.ID;
   return { tg, userName, tg_id };
 };
