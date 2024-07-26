ac4a3ee4d2b58dc57641cc9eadc39c216a4b5378
unknown <UstinovichSergey>
2024-07-26 13:48:56 +0300

    referal

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     <>
       <Suspense fallback={<div>Loading</div>}>
         <Routes>
-          <Route path={'*'} element={<Layout />}>
+          <Route path={'/'} element={<Layout />}>
             <Route path={"friends"} element={<div className='friend'>Ntfrf</div>} />
           </Route>
         </Routes>
