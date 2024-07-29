5fe51ae1b21c05c97e647d0cd457cec0b474a84f
unknown <UstinovichSergey>
2024-07-29 17:39:08 +0300

    fixHttpS

--- src/provider/telegram/telegram.ts
+++ src/provider/telegram/telegram.ts
@@ -8,7 +8,7 @@ export const useTelegram = () => {
   const tg = window.Telegram.WebApp;
   // const userName = tg?.initDataUnsafe?.user?.username;
   // const tg_id = tg?.initDataUnsafe?.user?.id
-  const tg_id = '0987654321'
+  const tg_id = '12345'
   const userName = 'Serge'
   return { tg, userName, tg_id };
 };
