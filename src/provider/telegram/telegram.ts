e2a1ba47855fac7aee8b28afb50c63a9b8eeb1fd
unknown <UstinovichSergey>
2024-08-01 13:59:34 +0300

    fixHeader

--- src/provider/telegram/telegram.ts
+++ src/provider/telegram/telegram.ts
@@ -10,7 +10,7 @@ export const useTelegram = () => {
   // const userName = tg?.initDataUnsafe?.user?.username;
   // const tg_id = tg?.initDataUnsafe?.user?.id
   
-  // tg.enableVerticalSwipes() 
+  tg.setHeaderColor("#000", "#fff");
   const tg_id = '12345'
   const userName = 'Сергей'
   return { tg, userName, tg_id };
