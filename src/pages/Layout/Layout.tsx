7936ee190ee8e2ce4d97543196c921a16077d3ca
v1ssar1on <122610076+v1ssar1on@users.noreply.github.com>
2024-09-15 04:35:50 +0300

    update btnGemaField

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,30 +1,51 @@
 import style from "./Layout.module.scss";
 import CoinsDiamond from "../../components/CoinsDiamond/CoinsDiamond";
 import NavBar from "../../components/NavBar/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import GameField from "../../components/GameField/GameField";
 import BtnGemaField from "../../components/BtnGemaField/BtnGemaField";
-import imgTasks from '../../assets/img/tasks.png'
-import imgUpgrades from '../../assets/img/upgrades.png'
-import imgBoosters from '../../assets/img/boosters.png'
+import imgTasks from "../../assets/img/tasks.png";
+import imgUpgrades from "../../assets/img/upgrades.png";
+import imgBoosters from "../../assets/img/boosters.png";
 import { Toaster } from "react-hot-toast";
 
-
 function Layout() {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    navigate(path);
+  };
+
   return (
     <>
       <header className={style.headers}>
-        <Toaster
-          position="top-center"
-          reverseOrder={false}
-        />
+        <Toaster position="top-center" reverseOrder={false} />
         <CoinsDiamond />
       </header>
       <main className={style.main}>
         <GameField />
-        <BtnGemaField text={'Tasks'} img={imgTasks} path={'/tasks'} bottonP={'35'} left={'3'} />
-        <BtnGemaField text={'Upgrades'} img={imgUpgrades} path={'/upgrades'} bottonP={'24'} left={'3'} />
-        <BtnGemaField text={'Boosters'} img={imgBoosters} path={'/boosters'} bottonP={'24'} right={'3'} />
+        <BtnGemaField
+          text={"Tasks"}
+          img={imgTasks}
+          path={"/tasks"}
+          bottonP={"35"}
+          left={"3"}
+        />
+        <BtnGemaField
+          text={"Upgrades"}
+          img={imgUpgrades}
+          path={"/upgrades"}
+          bottonP={"24"}
+          left={"3"}
+          onClick={() => handleNavigate("upgrades/army")}
+        />
+        <BtnGemaField
+          text={"Boosters"}
+          img={imgBoosters}
+          path={"/boosters"}
+          bottonP={"24"}
+          right={"3"}
+        />
         <Outlet />
       </main>
       <footer className={style.footers}>
