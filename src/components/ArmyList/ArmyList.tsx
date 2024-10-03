f5e43e782474851a13e199ee783eb9b237e065fb
maksimjoyharper <makksss1123@icloud.com>
2024-10-03 22:28:17 +0300

    feat: cards army styles

--- src/components/ArmyList/ArmyList.tsx
+++ src/components/ArmyList/ArmyList.tsx
@@ -1,6 +1,8 @@
 import style from "./ArmyList.module.scss";
 import { ArmyType } from "../../types/ArmyType";
+import iconLevel from "../../assets/img/level.png";
 import { Link, Outlet } from "react-router-dom";
+import imgCards from "../../assets/img/card icon.png";
 
 interface ArmyList {
   army?: ArmyType[];
@@ -22,6 +24,16 @@ function ArmyList({ army }: ArmyList) {
                   src={item.image}
                   alt={item.name}
                 />
+                <div className={style.box_lvl}>
+                  {" "}
+                  <img className={style.shield} src={iconLevel} />
+                  <p className={style.title_lvl}>8</p>
+                </div>
+                <div className={style.box_progress_lvl}>
+                  <img className={style.imgCards} src={imgCards} />
+                  <div className={style.progress_lvl}></div>
+                  <span className={style.text_progress_lvl}>8/14</span>
+                </div>
               </Link>
             </li>
           ))}
