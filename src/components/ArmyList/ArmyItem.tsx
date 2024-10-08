09150a9eed0e877ac0230bc024ae9e37985f5e80
v1ssar1on <122610076+v1ssar1on@users.noreply.github.com>
2024-10-08 05:02:01 +0300

    upd

--- src/components/ArmyList/ArmyItem.tsx
+++ src/components/ArmyList/ArmyItem.tsx
@@ -1,37 +1,37 @@
-import { useDispatch, useSelector } from "react-redux";
-import style from "./ArmyList.module.scss";
-import { getCoin } from "../../provider/StoreProvider/selectors/getCoin";
-import { useTelegram } from "../../provider/telegram/telegram";
-import { useMutation } from "@tanstack/react-query";
-import { upDamage, upSpeed } from "../../api/userInfo";
-import { coinActions } from "../../provider/StoreProvider";
-import { queryClient } from "../../api/queryClient";
-import { getArmy } from "../../provider/StoreProvider/selectors/getArmy";
-import { useNavigate, useParams } from "react-router-dom";
-import { Button } from "../../ui/Button";
-import ModalRoute from "../../ui/ModalRoute/ModalRoute";
-import ProgressBar from "../../ui/ProgressBar/ProgressBar";
-import level from "../../assets/img/level.png";
-import { ArmyCloseSvg } from "../../assets/svg/ArmyCloseSvg";
+import { useDispatch, useSelector } from 'react-redux'
+import style from './ArmyList.module.scss'
+import { getCoin } from '../../provider/StoreProvider/selectors/getCoin'
+import { useTelegram } from '../../provider/telegram/telegram'
+import { useMutation } from '@tanstack/react-query'
+import { upDamage, upSpeed } from '../../api/userInfo'
+import { coinActions } from '../../provider/StoreProvider'
+import { queryClient } from '../../api/queryClient'
+import { getArmy } from '../../provider/StoreProvider/selectors/getArmy'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Button } from '../../ui/Button'
+import ModalRoute from '../../ui/ModalRoute/ModalRoute'
+import ProgressBar from '../../ui/ProgressBar/ProgressBar'
+import level from '../../assets/img/level.png'
+import upgradeBack from '../../assets/img/upgradeBack.png'
 
 export const ArmyItem = () => {
   // const [unitArr, setUnitArr] = useState<ArmyType[]>();
   // const [unit, setUnit] = useState<ArmyType>();
-  const infoUser = useSelector(getCoin);
-  const { tg_id } = useTelegram();
-  const dispatch = useDispatch();
-  const armyUser = useSelector(getArmy);
-  const { id } = useParams();
-  const navigate = useNavigate();
+  const infoUser = useSelector(getCoin)
+  const { tg_id } = useTelegram()
+  const dispatch = useDispatch()
+  const armyUser = useSelector(getArmy)
+  const { id } = useParams()
+  const navigate = useNavigate()
 
   const handleBack = () => {
-    navigate("/upgrades/army");
-  };
+    navigate('/upgrades/army')
+  }
 
   if (armyUser === undefined) {
-    return null;
+    return null
   }
-  const unit = armyUser.find((item) => item.id_warrior === Number(id));
+  const unit = armyUser.find((item) => item.id_warrior === Number(id))
 
   // useEffect(() => {
   //   if (armyUser) {
@@ -50,40 +50,40 @@ export const ArmyItem = () => {
       mutationFn: (data: { tg_id: string; id_warrior: number }) =>
         upDamage(data.tg_id, data.id_warrior),
       onSuccess: (data) => {
-        const moneyCoin: number = data.money;
-        dispatch(coinActions.updateCoinMinus(moneyCoin));
-        queryClient.invalidateQueries({ queryKey: ["army", tg_id] });
+        const moneyCoin: number = data.money
+        dispatch(coinActions.updateCoinMinus(moneyCoin))
+        queryClient.invalidateQueries({ queryKey: ['army', tg_id] })
       },
     },
     queryClient
-  );
+  )
 
   const upSpeedMutate = useMutation(
     {
       mutationFn: (data: { tg_id: string; id_warrior: number }) =>
         upSpeed(data.tg_id, data.id_warrior),
       onSuccess: (data) => {
-        const moneyCoin: number = data.money;
-        dispatch(coinActions.updateCoinMinus(moneyCoin));
-        queryClient.invalidateQueries({ queryKey: ["army", tg_id] });
+        const moneyCoin: number = data.money
+        dispatch(coinActions.updateCoinMinus(moneyCoin))
+        queryClient.invalidateQueries({ queryKey: ['army', tg_id] })
       },
     },
     queryClient
-  );
+  )
 
   const handleUpDamage = (id_warrior: number) => {
-    upDamageMutate.mutate({ tg_id, id_warrior });
-  };
+    upDamageMutate.mutate({ tg_id, id_warrior })
+  }
 
   const handleUpSpeed = (id_warrior: number) => {
-    upSpeedMutate.mutate({ tg_id, id_warrior });
-  };
+    upSpeedMutate.mutate({ tg_id, id_warrior })
+  }
 
   const lvlInBar = () => {
     if (unit?.cards && unit.max_cards) {
-      return (unit.cards * 100) / unit.max_cards;
+      return (unit.cards * 100) / unit.max_cards
     }
-  };
+  }
 
   return (
     <ModalRoute
@@ -93,7 +93,7 @@ export const ArmyItem = () => {
     >
       <div className={style.upgradeItem}>
         {unit && (
-          <div style={{ height: "100%", overflow: "hidden" }}>
+          <div style={{ height: '100%', overflow: 'hidden' }}>
             <p className={style.upgradeCP}>CP 253</p>
             <ProgressBar className={style.progressUnit} value={30} max={100} />
             <div className={style.upgradeDown}>
@@ -105,18 +105,33 @@ export const ArmyItem = () => {
               >
                 <img src={level} alt="" />
               </ProgressBar>
-              <Button className={style.upgradeEvolve}>Evolve</Button>
+              <Button
+                isDisabled={unit.cards < unit.max_cards}
+                className={style.upgradeEvolve}
+              >
+                Evolve
+              </Button>
               <div>
                 <ul className={style.upgradeListEvolve}>
                   <li className={style.upgradeListItem}>
                     <strong>{unit.lvl_speed} speed lvl</strong>
-                    <Button onClick={() => handleUpSpeed(unit.id_warrior)}>
-                      {unit.price_speed}
-                    </Button>
+                    {infoUser?.money ? (
+                      <Button
+                        isDisabled={infoUser.money < unit.price_speed}
+                        onClick={() => handleUpSpeed(unit.id_warrior)}
+                      >
+                        {unit.price_speed}
+                      </Button>
+                    ) : (
+                      <p>Loading...</p>
+                    )}
                   </li>
                   <li className={style.upgradeListItem}>
                     <strong>{unit.lvl_damage} damage lvl</strong>
-                    <Button onClick={() => handleUpDamage(unit.id_warrior)}>
+                    <Button
+                      isDisabled={infoUser?.money < unit.price_damage}
+                      onClick={() => handleUpDamage(unit.id_warrior)}
+                    >
                       {unit.price_damage}
                     </Button>
                   </li>
@@ -126,9 +141,9 @@ export const ArmyItem = () => {
           </div>
         )}
         <Button onClick={handleBack} className={style.closeItem}>
-          <ArmyCloseSvg />
+          <img src={upgradeBack} alt="домой" />
         </Button>
       </div>
     </ModalRoute>
-  );
-};
+  )
+}
