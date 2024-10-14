cd4fe9001edef1eef70d9fc7a95071adeaa9ddf5
v1ssar1on <122610076+v1ssar1on@users.noreply.github.com>
2024-10-15 02:49:45 +0300

    modified

--- src/pages/Awards/AwardsItem.tsx
+++ src/pages/Awards/AwardsItem.tsx
@@ -1,27 +1,35 @@
 import style from './Awards.module.scss'
-import Modal from "../../ui/Modal/Modal.tsx";
-import {Button} from "../../ui/Button";
-import coinMoney from "../../assets/img/coinMoney.png";
-import diamondMoney from "../../assets/img/diamondMoney.png";
+import Modal from '../../ui/Modal/Modal.tsx'
+import { Button } from '../../ui/Button'
+import coinMoney from '../../assets/img/coinMoney.png'
+import diamondMoney from '../../assets/img/diamondMoney.png'
 import yourReward from '../../assets/img/yourReward.png'
+import treeChest from '../../assets/img/bigChest.png'
+import rotateLuis from '../../assets/img/rotateLuis.png'
 
 interface AwardsProps {
   isOpen: boolean
   onClose: () => void
 }
 
-export const AwardsItem = ({isOpen, onClose} : AwardsProps) => {
+export const AwardsItem = ({ isOpen, onClose }: AwardsProps) => {
   return (
     <Modal lazy isOpen={isOpen} onClose={onClose}>
       <div className={style.awardsItemBlock}>
-        <img src={yourReward} alt=""/>
+        <div className={style.yourRewardBlock}>
+          <img className={style.chestAwards} src={treeChest} alt="" />
+          <div className={style.rotateBlock}>
+            <img className={style.rotate} src={rotateLuis} alt="" />
+          </div>
+          <img style={{ zIndex: '5' }} src={yourReward} alt="" />
+        </div>
         <div className={style.awardsItems}>
           <div className={style.awardsGridItem}>
-            <img src={coinMoney} alt=""/>
+            <img src={coinMoney} alt="" />
             <p>50.000</p>
           </div>
           <div className={style.awardsGridItem}>
-            <img src={diamondMoney} alt=""/>
+            <img src={diamondMoney} alt="" />
             <p>500</p>
           </div>
         </div>
