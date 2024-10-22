be4fb2cbb8ab90e7dad9f02d1c007f45b5edd0e0
v1ssar1on <122610076+v1ssar1on@users.noreply.github.com>
2024-10-22 17:39:56 +0300

    remarked

--- src/provider/telegram/telegram.ts
+++ src/provider/telegram/telegram.ts
@@ -1,16 +1,16 @@
 declare global {
   interface Window {
-    Telegram: any;
+    Telegram: any
   }
 }
 
 export const useTelegram = () => {
-  const tg = window.Telegram.WebApp;
+  const tg = window.Telegram.WebApp
 
-  const userName = tg?.initDataUnsafe?.user?.username;
-  const tg_id = tg?.initDataUnsafe?.user?.id;
-  // const tg_id = "12323";
-  // const userName = "byngra";
+  // const userName = tg?.initDataUnsafe?.user?.username;
+  // const tg_id = tg?.initDataUnsafe?.user?.id;
+  const tg_id = '12323'
+  const userName = 'byngra'
   // const idRef = '87654321'
-  return { tg, userName, tg_id };
-};
+  return { tg, userName, tg_id }
+}
